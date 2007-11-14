b84e18f63e2ec83859c5c4f71188f8aa271a48b5
perry <perry@bed332bc-0dc3-4ae0-abcf-29c26e6865f6>
2007-11-14 03:58:32 +0000

    Check if an interface is really an ethernet interface before renaming
    git-svn-id: file:///tmp/netmangler/trunk@3 bed332bc-0dc3-4ae0-abcf-29c26e6865f6

--- lib/iftype-ethernet.sh
+++ lib/iftype-ethernet.sh
@@ -9,7 +9,9 @@ iftype_ethernet() {
 	else
 		OLDNAME=$(ip -o link |
 			grep -i "^[0-9]*: [^.:]*: .* link/ether $MAC" |
-			sed 's/^[0-9]*: \([^.:]*\): .*/\1/')
+			sed 's/^[0-9]*: \([^.:]*\): .*/\1/' |
+			while read int; do [ ! -d /proc/sys/dev/$int -a ! -f /prr
+			oc/net/madwifi/$int ] && echo $int; done)
 
 		echo $IFNAME: configuring interface with mac=$MAC as $OLDNAME =\> $IFNAME
 		ip link set "$OLDNAME" down
