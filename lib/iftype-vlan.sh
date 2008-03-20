822ab4dac782f145f6cf7fe7e4034a1f3dc0da55
perry <perry@bed332bc-0dc3-4ae0-abcf-29c26e6865f6>
2008-03-20 00:37:05 +0000

    If vlan_tag isn't given, then guess it from the filename
    git-svn-id: file:///tmp/netmangler/trunk@27 bed332bc-0dc3-4ae0-abcf-29c26e6865f6

--- lib/iftype-vlan.sh
+++ lib/iftype-vlan.sh
@@ -8,6 +8,8 @@ vlan_tag(){
 
 iftype_vlan() {
 	for i in $PARENTIFS; do
+		VLANNAME=${IFNAME#*.}
+		VLAN=${VLAN:-$VLANNAME}
 		#VLAN=${IFNAME#*.} #Took this out so we can configure VLAN tags without changing the NM filename
 		echo $IFNAME: Configuring $IFNAME vlan $VLAN with a master interface of $i
 		ERR=$(vconfig set_name_type DEV_PLUS_VID_NO_PAD) || echo $ERR >&2
