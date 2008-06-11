00d669607ff5ce0edc4211cb70d5a9cf6f0cc68a
scottr <scottr@bed332bc-0dc3-4ae0-abcf-29c26e6865f6>
2008-06-11 07:19:51 +0000

    Make dhcp a bit smarter again so that it can kill any running dhcp clients for the interface on cleanup.
    git-svn-id: file:///tmp/netmangler/trunk@49 bed332bc-0dc3-4ae0-abcf-29c26e6865f6

--- lib/addressing-dhcp.sh
+++ lib/addressing-dhcp.sh
@@ -2,11 +2,20 @@
 addressing_dhcp() {
 	echo $IFNAME: Starting dhcp listening on interface $IFNAME
 
-	if [ `which pump` != "" ]; then
-		pump -i $IFNAME &
-	elif [ `which udhcpc` != "" ]; then
-		udhcpc -b -i $IFNAME -t 20 &
+	DHCPC=""
+
+	if [ ! -z `which pump` ]; then
+		DHCPC="pump -i $IFNAME"
+	elif [ ! -z `which udhcpc` ]; then
+		DHCPC="udhcpc -b -i $IFNAME -t 20"
 	else
 		echo "No DHCP client!"
 	fi
+
+	if [ ! -z "${DHCPC}" ]; then 
+		${DHCPC} &
+		echo $! > /tmp/nm-dhcpc-${IFNAME}.pid
+		CLEANUP_CMDS="kill -9 \$(cat /tmp/nm-dhcpc-${IFNAME}.pid); $CLEANUP_CMDS"
+	fi
+	
 }
