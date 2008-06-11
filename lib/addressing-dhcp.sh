3e52b40a7322567f512e2344c898f1ae49e92edd
scottr <scottr@bed332bc-0dc3-4ae0-abcf-29c26e6865f6>
2008-06-11 05:04:18 +0000

    Make dhcp a bit smarter and use udhcpc if pump doesn't exist.
    git-svn-id: file:///tmp/netmangler/trunk@48 bed332bc-0dc3-4ae0-abcf-29c26e6865f6

--- lib/addressing-dhcp.sh
+++ lib/addressing-dhcp.sh
@@ -1,5 +1,12 @@
 
 addressing_dhcp() {
 	echo $IFNAME: Starting dhcp listening on interface $IFNAME
-	pump -i $IFNAME &
+
+	if [ `which pump` != "" ]; then
+		pump -i $IFNAME &
+	elif [ `which udhcpc` != "" ]; then
+		udhcpc -b -i $IFNAME -t 20 &
+	else
+		echo "No DHCP client!"
+	fi
 }
