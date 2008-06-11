747f82075d8613bdcedb017afacc55aa28adcd2c
scottr <scottr@bed332bc-0dc3-4ae0-abcf-29c26e6865f6>
2008-06-11 23:46:04 +0000

    pump forks itself so we can't rely on $! being accurate.
    git-svn-id: file:///tmp/netmangler/trunk@50 bed332bc-0dc3-4ae0-abcf-29c26e6865f6

--- lib/addressing-dhcp.sh
+++ lib/addressing-dhcp.sh
@@ -6,8 +6,12 @@ addressing_dhcp() {
 
 	if [ ! -z `which pump` ]; then
 		DHCPC="pump -i $IFNAME"
+		# pump doesnt write a pid and forks itself off, so we can't
+		# just rely on $! being accurate.
+		CLEANUP="ps a | grep [p]ump | grep ${IFNAME} | awk '{ print \$1 }'"
 	elif [ ! -z `which udhcpc` ]; then
 		DHCPC="udhcpc -b -i $IFNAME -t 20"
+		CLEANUP="cat /tmp/nm-dhcpc-${IFNAME}.pid"
 	else
 		echo "No DHCP client!"
 	fi
@@ -15,7 +19,7 @@ addressing_dhcp() {
 	if [ ! -z "${DHCPC}" ]; then 
 		${DHCPC} &
 		echo $! > /tmp/nm-dhcpc-${IFNAME}.pid
-		CLEANUP_CMDS="kill -9 \$(cat /tmp/nm-dhcpc-${IFNAME}.pid); $CLEANUP_CMDS"
+		CLEANUP_CMDS="kill -9 `\$(${CLEANUP})`; $CLEANUP_CMDS"
 	fi
 	
 }
