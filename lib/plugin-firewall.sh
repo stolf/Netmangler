3836a6479ee74447170f18309434a779f93e4a44
perry <perry@bed332bc-0dc3-4ae0-abcf-29c26e6865f6>
2008-06-17 01:03:22 +0000

    Add support for forward-in/forward-out chains
    git-svn-id: file:///tmp/netmangler/trunk@53 bed332bc-0dc3-4ae0-abcf-29c26e6865f6

--- lib/plugin-firewall.sh
+++ lib/plugin-firewall.sh
@@ -1,29 +1,36 @@
 
-init_firewall() {
-	echo Firewall: Plugin loaded
-}
-
 # Internal command to executa a command and output what failed
 _iptables() {
 	iptables $@ || echo Failed: iptables $@ 1>&2
 }
 
+init_firewall() {
+	echo Firewall: Plugin loaded
+	_iptables --table mangle --new-chain forward-out
+	# nat doesn't have a forward chain
+	_iptables --table filter --new-chain forward-out
+}
 
 newif_firewall() {
 	FWRULES=true
 
 	# mangle table rules
 	_iptables --table mangle --new-chain ${IFNAME}-prerouting
+	_iptables --table mangle --new-chain ${IFNAME}-in
+	_iptables --table mangle --new-chain ${IFNAME}-out
+	_iptables --table mangle --new-chain ${IFNAME}-postrouting
+	_iptables --table mangle --new-chain ${IFNAME}-firewall-in
+	_iptables --table mangle --new-chain ${IFNAME}-firewall-out
+
 	_iptables --table mangle --insert PREROUTING --in-interface $IFNAME --jump ${IFNAME}-prerouting
 
-	_iptables --table mangle --new-chain ${IFNAME}-in
 	_iptables --table mangle --insert INPUT --in-interface $IFNAME --jump ${IFNAME}-in
 
-	_iptables --table mangle --new-chain ${IFNAME}-out
 	_iptables --table mangle --insert OUTPUT --out-interface $IFNAME --jump ${IFNAME}-out
 
-	_iptables --table mangle --new-chain ${IFNAME}-postrouting
 	_iptables --table mangle --insert POSTROUTING --out-interface $IFNAME ${IFNAME}-postrouting
+	_iptables --table mangle --insert FORWARD --in-interface $IFNAME ${IFNAME}-forward-in
+	_iptables --table mangle --insert forward-out --out-interface $IFNAME ${IFNAME}-forward-out
 
 	# NAT table rule
 	_iptables --table nat --new-chain ${IFNAME}-prerouting
@@ -36,8 +43,12 @@ newif_firewall() {
 	# Filter table rules
 	_iptables --table filter --new-chain ${IFNAME}-in
 	_iptables --table filter --new-chain ${IFNAME}-out
+	_iptables --table filter --new-chain ${IFNAME}-forward-in
+	_iptables --table filter --new-chain ${IFNAME}-forward-out
 	_iptables --table filter --insert INPUT --in-interface $IFNAME --jump ${IFNAME}-in
 	_iptables --table filter --insert OUTPUT --out-interface $IFNAME --jump ${IFNAME}-out
+	_iptables --table filter --insert FORWARD --in-interface $IFNAME --jump ${IFNAME}-forward-in
+	_iptables --table filter --insert forward-out --out-interface $IFNAME --jump ${IFNAME}-forward-out
 }
 
 # usage:
@@ -53,6 +64,9 @@ firewall() {
 	shift
 	target=$1
 	shift
+	if [ "$chain" == "${IFNAME}-forward-in" -a "$target" == "ACCEPT"]; then
+		target="forward-out"
+	fi
 	case $table in
 		ip-*)
 			FWRULES="$FWRULES; iptables --table ${table#ip-} --append $chain --jump $target $@"
@@ -74,6 +88,19 @@ do_firewall() {
 	# Execute rules
 	eval $FWRULES
 	CLEANUP_CMDS="
+	_iptables --table mangle --delete PREROUTING --in-interface $IFNAME --jump ${IFNAME}-prerouting;
+	_iptables --table mangle --delete INPUT --in-interface $IFNAME --jump ${IFNAME}-in;
+	_iptables --table mangle --delete OUTPUT --out-interface $IFNAME --jump ${IFNAME}-out;
+	_iptables --table mangle --delete POSTROUTING --out-interface $IFNAME ${IFNAME}-postrouting;
+	_iptables --table mangle --delete FORWARD --in-interface $IFNAME ${IFNAME}-forward-in;
+	_iptables --table mangle --delete forward-out --out-interface $IFNAME ${IFNAME}-forward-out;
+	_iptables --table mangle --delete-chain ${IFNAME}-prerouting;
+	_iptables --table mangle --delete-chain ${IFNAME}-in;
+	_iptables --table mangle --delete-chain ${IFNAME}-out;
+	_iptables --table mangle --delete-chain ${IFNAME}-postrouting;
+	_iptables --table mangle --delete-chain ${IFNAME}-forward-in;
+	_iptables --table mangle --delete-chain ${IFNAME}-forward-out;
+
 	_iptables --table nat --delete OUTPUT --out-interface $IFNAME --jump ${IFNAME}-out;
 	_iptables --table nat --delete PREROUTING --in-interface $IFNAME --jump ${IFNAME}-prerouting;
 	_iptables --table nat --delete POSTROUTING --out-interface $IFNAME --jump ${IFNAME}-postrouting;
@@ -82,7 +109,11 @@ do_firewall() {
 	_iptables --table nat --delete-chain ${IFNAME}-postrouting;
 	_iptables --table filter --delete INPUT --in-interface $IFNAME --jump ${IFNAME}-in;
 	_iptables --table filter --delete OUTPUT --out-interface $IFNAME --jump ${IFNAME}-out;
+	_iptables --table filter --delete FORWARD --in-interface $IFNAME --jump ${IFNAME}-forward-in;
+	_iptables --table filter --delete forward-out --out-interface $IFNAME --jump ${IFNAME}-forward-out;
 	_iptables --table filter --delete-chain ${IFNAME}-in;
 	_iptables --table filter --delete-chain ${IFNAME}-out;
+	_iptables --table filter --delete-chain ${IFNAME}-forward-in;
+	_iptables --table filter --delete-chain ${IFNAME}-forward-out;
 	$CLEANUP_CMDS"
 }
