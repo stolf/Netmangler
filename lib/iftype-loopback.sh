4dd794b2f53c38238dd99cd475d49dd2cf037da9
perry <perry@bed332bc-0dc3-4ae0-abcf-29c26e6865f6>
2008-12-16 05:59:56 +0000

    Bring up lookback interfaces
    git-svn-id: file:///tmp/netmangler/trunk@60 bed332bc-0dc3-4ae0-abcf-29c26e6865f6

--- lib/iftype-loopback.sh
+++ lib/iftype-loopback.sh
@@ -2,6 +2,6 @@
 iftype_loopback() {
 	if [ ! -d /proc/sys/net/ipv4/conf/$IFNAME ]; then
 		echo $IFNAME: Creating $IFNAME
-		# ip link set name $IFNAME dev lo
+		ip link set name $IFNAME dev lo
 	fi
 }
